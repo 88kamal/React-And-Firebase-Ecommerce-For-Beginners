7c5bdb89b8cd32a893598e128cbd8c6046468c11
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2024-02-20 16:25:33 +0530

    done

--- src/pages/allProduct/AllProduct.jsx
+++ src/pages/allProduct/AllProduct.jsx
@@ -5,13 +5,14 @@ import myContext from "../../context/myContext";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import { addToCart, deleteFromCart } from "../../redux/cartSlice";
+import Loader from "../../components/loader/Loader";
 
 
 const AllProduct = () => {
     const navigate = useNavigate();
 
     const context = useContext(myContext);
-    const {getAllProduct} = context;
+    const {loading,getAllProduct} = context;
 
     const cartItems = useSelector((state) => state.cart);
     const dispatch = useDispatch();
@@ -43,15 +44,18 @@ const AllProduct = () => {
             {/* main  */}
             <section className="text-gray-600 body-font">
                 <div className="container px-5 lg:px-0 py-5 mx-auto">
+                <div className="flex justify-center">
+                            {loading && <Loader/>}
+                        </div>
                     <div className="flex flex-wrap -m-4">
                     {getAllProduct.map((item, index) => {
                             const { id, title, price, productImageUrl } = item
                             return (
-                                <div key={index} className="p-4  w-full md:w-1/4">
+                                <div key={index} className="p-4 w-full md:w-1/4">
                                     <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
                                         <img
                                             onClick={() => navigate(`/productinfo/${id}`)}
-                                            className="lg:h-80  h-96 w-full p-2 rounded-2xl"
+                                            className="lg:h-80  h-96 w-full"
                                             src={productImageUrl}
                                             alt="blog"
                                         />
@@ -74,7 +78,7 @@ const AllProduct = () => {
                                                 <button
                                                     onClick={() => deleteCart(item)}
                                                     className=" bg-red-700 hover:bg-pink-600 w-full text-white py-[4px] rounded-lg font-bold">
-                                                    Delete To Cart
+                                                    Delete From Cart
                                                 </button>
 
                                                 : 
