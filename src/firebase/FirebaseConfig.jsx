7a383dc7d0fcc251f473ed533ef894b62ecb7199
Kamal Nayan Upadhyay <93386296+88kamal@users.noreply.github.com>
2024-06-08 08:57:35 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDBHTUtOeuCON7AEF3UPPxU1sHsnFfBv-M",
-    authDomain: "ecommerce-for.firebaseapp.com",
-    projectId: "ecommerce-for",
-    storageBucket: "ecommerce-for.appspot.com",
-    messagingSenderId: "609664686595",
-    appId: "1:609664686595:web:302bf25cc8efa64a4e4c36"
+  apiKey: "AIzaSyBQsJ5MS4-MacHnO5wxQX8V2tGe0y6eCXo",
+  authDomain: "myecommerce2-7b8fc.firebaseapp.com",
+  projectId: "myecommerce2-7b8fc",
+  storageBucket: "myecommerce2-7b8fc.appspot.com",
+  messagingSenderId: "715233167432",
+  appId: "1:715233167432:web:3a0acec83b123399d3307d"
 };
 
 // Initialize Firebase
